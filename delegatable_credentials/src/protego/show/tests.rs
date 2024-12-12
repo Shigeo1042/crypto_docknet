d3b73cd3a98ed9906b7c9756855921f7e1211df5
Shigeo1042 <shigeo.mizuno1024@gmail.com>
2024-12-13 04:26:47 +0900

    add some files

--- delegatable_credentials/src/protego/show/tests.rs
+++ delegatable_credentials/src/protego/show/tests.rs
@@ -632,3 +632,121 @@ fn credential_show_with_delegation_policy_and_revocation() {
         0,
     );
 }
+
+#[test]
+pub fn my_test(){
+    use dock_crypto_utils::elgamal::{SecretKey as AuditorSecretKey, PublicKey as AuditorPublicKey};
+    use rand::{self, Rng};
+    use crate::protego::keys::{IssuerSecretKey, IssuerPublicKey, UserSecretKey, UserPublicKey};
+    use crate::protego::issuance;
+    // let message_len = [5, 10, 50, 100, 500, 1000, 5000, 10000];
+    let message_len = [5, 10];
+    // let issuer_num = [5, 10, 50, 100];
+    let issuer_num = [5, 10];
+    let mut rng =  StdRng::seed_from_u64(0u64);
+
+    for message_len_temp in message_len.iter(){
+        let (set_comm_srs, _) = SetCommitmentSRS::<Bls12_381>::generate_with_random_trapdoor::<StdRng, Blake2b512>(&mut rng, *message_len_temp, Some("Issuer-Hiding".as_bytes()));
+                
+        let mut message_fr_temp: Vec<Fr> = Vec::new();
+        for _ in 0..*message_len_temp{
+            message_fr_temp.push(Fr::rand(&mut rng));
+        }
+
+        let open_message_6 = message_len_temp * 3 / 5;
+        let mut open_message_len_temp = Vec::new();
+        if open_message_6 == 3{
+            open_message_len_temp.append(&mut [3, message_len_temp - 3].to_vec());
+        }else{
+            open_message_len_temp.append(&mut [3, open_message_6, message_len_temp - 3].to_vec());
+        }
+        open_message_len_temp.sort();
+        
+        for open_message_len_i in open_message_len_temp.iter(){
+
+            for issuer_num_temp in issuer_num.iter(){
+                let mut open_temp = Vec::new();
+                for j in 0..*open_message_len_i{
+                    let mut flg = true;
+                    let mut x = rng.gen_range(0..*message_len_temp) as usize;
+                    while flg {
+                        flg = false;
+                        for i in 0..j{
+                            if x == open_temp[i as usize]{
+                                flg = true;
+                                x = rng.gen_range(0..*message_len_temp) as usize;
+                                break;
+                            }
+                        }
+                    }
+                    open_temp.push(x);
+                }
+                open_temp.sort();
+
+                let isk = IssuerSecretKey::<Bls12_381>::new(&mut rng, false, false).unwrap();
+                let ipk = IssuerPublicKey::<Bls12_381>::new(&isk, &set_comm_srs.get_P2());
+        
+                let vsk = DelegationPolicySecretKey::<Bls12_381>::new(&mut rng, issuer_num_temp.clone() as u32).unwrap();
+                let vpk = DelegationPolicyPublicKey::<Bls12_381>::new(&vsk, set_comm_srs.get_P1());
+    
+                let usk = UserSecretKey::<Bls12_381>::new(&mut rng, false);
+                let upk = UserPublicKey::<Bls12_381>::new(&usk, set_comm_srs.get_P1());
+        
+                let ask = AuditorSecretKey::new(&mut rng);
+                let apk = AuditorPublicKey::new(&ask, set_comm_srs.get_P1());
+
+                let prep_set_comm_srs = PreparedSetCommitmentSRS::<Bls12_381>::from(set_comm_srs.clone());
+                let prep_ipk = PreparedIssuerPublicKey::<Bls12_381>::from(ipk.clone());
+                let sig_req_p = issuance::SignatureRequestProtocol::<Bls12_381>::init(&mut rng, &usk, false, &set_comm_srs.get_P1());
+                let mut chal_bytes = vec![];
+                sig_req_p.challenge_contribution(&upk, &set_comm_srs.get_P1(), None, &mut chal_bytes).unwrap();
+                let challenge = schnorr_pok::compute_random_oracle_challenge::<Fr, Blake2b512>(&chal_bytes);
+                let (sig_req, sig_req_opn) = sig_req_p.gen_request(&mut rng, message_fr_temp.clone(), &usk, &challenge, &set_comm_srs).unwrap();
+                sig_req.verify(message_fr_temp.clone(), &upk, &challenge, None, None, set_comm_srs.clone()).unwrap();
+                let sig = sig_req.clone().sign(&mut rng, &isk, false.then_some(&upk), false.then_some(&apk), &set_comm_srs.get_P1(), &set_comm_srs.get_P2()).unwrap();
+                let cred = issuance::Credential::<Bls12_381>::new(sig_req.clone(), sig_req_opn.clone(), sig.clone(), message_fr_temp.clone(), prep_ipk.clone(), false.then_some(&upk), false.then_some(&apk), &set_comm_srs.get_P1(), prep_set_comm_srs.clone().prepared_P2).unwrap();
+    
+                let mut trusted_issuers = Vec::new();
+                for _ in 0..*issuer_num_temp{
+                    let isk_temp = IssuerSecretKey::<Bls12_381>::new(&mut rng, false, false).unwrap();
+                    let ipk_temp = IssuerPublicKey::<Bls12_381>::new(&isk_temp, &set_comm_srs.get_P2());
+                    trusted_issuers.push(ipk_temp);
+                }
+                let r = rng.gen_range(1..*issuer_num_temp);
+                trusted_issuers[r] = ipk.clone();
+                let mut trusted_issuer_list = Vec::new();
+                for i in 0..*issuer_num_temp{
+                    trusted_issuer_list.push((&trusted_issuers[i], DelegationPolicySecretKey::<Bls12_381>::sign_public_key(&vsk, &mut rng, &trusted_issuers[i], &set_comm_srs.get_P1(), &set_comm_srs.get_P2()).unwrap()));
+                }
+                let nonce = vec![1, 2, 3];
+                let mut trusted_issuer_sig = trusted_issuer_list[0].clone();
+                for trusted_issuer in trusted_issuer_list.iter(){
+                    if *trusted_issuer.0 == ipk{
+                        trusted_issuer_sig = trusted_issuer.clone();
+                    }
+                }
+                assert!(trusted_issuer_sig.0 == &ipk);
+                let mut message_open_temp = Vec::new();
+                for i in open_temp.iter(){
+                    message_open_temp.push(message_fr_temp[*i]);
+                }
+                let show_proto = CredentialShowProtocolWithDelegationPolicy::<Bls12_381>::init(&mut rng, cred.clone(), message_open_temp.clone(), &ipk, &trusted_issuer_sig.1, false.then_some(&upk), false.then_some(&apk), &set_comm_srs).unwrap();
+
+                let mut chal_bytes = vec![];
+                show_proto.challenge_contribution(None, None, None, &set_comm_srs.get_P1(), &nonce, &mut chal_bytes).unwrap();
+                let challenge = schnorr_pok::compute_random_oracle_challenge::<Fr, Blake2b512>(&chal_bytes);
+                let show = show_proto.gen_show(None, &challenge).unwrap();
+        
+                let bench_name = format!(
+                    "Verify_Present_messagelen{}_issuernum{}_openmessage{}",
+                    message_len_temp, issuer_num_temp, open_message_len_i
+                );
+                println!("{}", bench_name);
+                let verify = show.verify(&challenge, message_open_temp.clone(), &vpk, false.then_some(&apk), prep_set_comm_srs.clone()).unwrap();
+                // println!("Verify: {}", verify);
+                println!("test");
+                // assert!(verify == true);
+            }
+        }
+    }
+}
